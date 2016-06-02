3f1b0ebcd162e6852d75d98e495c8b83343bd421
Naelson Douglas <naelson17@gmail.com>
2016-06-02 01:57:38 -0300

    a syntax update

--- src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
+++ src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
@@ -1,7 +1,11 @@
 package FirstHibernate.com.myhib.CRUDS;
 
+import java.util.List;
+
 import org.hibernate.Hibernate;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
 
@@ -9,29 +13,37 @@ public class CrudsOps {
 
 	public static void main(String[] args) {
 			
-		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+SessionFactory factory = new Configuration().configure().buildSessionFactory();	
+		
+		Session session = factory.openSession();		
 		
 		
-		System.out.println("Configurations ran");
-		Session session = sf.openSession();
-		session.beginTransaction();
-		System.out.println("Transaction began");
+		Transaction t =  (Transaction) session.beginTransaction();
+		
+		Contacts p = new Contacts();		
+		p.setId(3);
+		p.setFname("Naelson Douglasasdasd");
+		p.setLname("Douglas2");		
+		
+		session.save(p);
+		session.getTransaction().commit();
+		
+		
+		Query query = session.createQuery("from Contacts");		
+		System.out.println(query.list().size());
+		
+		List<Contacts>  users =   query.list();
+		
+		for (Contacts temp : users){
+			System.out.println(temp.getFname());
+		}
 		
 		
 		
-	
-        
-        Contacts cont = new Contacts();
-        cont.setId(2);
-        cont.setFname("naelson_hib");
-        cont.setLname("douglas_hibernate");
+		session.close();
+		
          
-        //Save the employee in database
-        session.save(cont);
- 
-        //Commit the transaction
-        session.getTransaction().commit();
-		
+      		
 			
 	}
 
