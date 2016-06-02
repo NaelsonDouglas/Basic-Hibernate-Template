d751eea20c69ab9af4ca8194e43827dafddf0488
Naelson Douglas <naelson17@gmail.com>
2016-06-02 02:07:59 -0300

    removed a bug caused by git

--- src/main/java/FirstHibernate/com/myhib/CRUDS/Contacts.java
+++ src/main/java/FirstHibernate/com/myhib/CRUDS/Contacts.java
@@ -1,40 +1,6 @@
 package FirstHibernate.com.myhib.CRUDS;
 
 public class Contacts {
-<<<<<<< HEAD
-	int id;
-	String fname;
-	String lname;
-	
-	
-	
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFname() {
-		return fname;
-	}
-	public void setFname(String fname) {
-		this.fname = fname;
-	}
-	public String getLname() {
-		return lname;
-	}
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
-	
-	
-	
-	
-	
-	
-=======
 	
 	int id;
 	String fname;	
@@ -77,5 +43,4 @@ public class Contacts {
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
->>>>>>> ab06cfd3286c38712a045a36cb750825862aa177
 }
--- src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
+++ src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
@@ -2,34 +2,18 @@ package FirstHibernate.com.myhib.CRUDS;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
-=======
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
->>>>>>> ab06cfd3286c38712a045a36cb750825862aa177
 
 public class CrudsOps {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 			
 SessionFactory factory = new Configuration().configure().buildSessionFactory();	
-=======
-		
-		
-
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();	
->>>>>>> ab06cfd3286c38712a045a36cb750825862aa177
 		
 		Session session = factory.openSession();		
 		
@@ -38,11 +22,7 @@ SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		
 		Contacts p = new Contacts();		
 		p.setId(3);
-<<<<<<< HEAD
-		p.setFname("Naelson Douglasasdasd");
-=======
-		p.setFname("Naelson Douglas");
->>>>>>> ab06cfd3286c38712a045a36cb750825862aa177
+		p.setFname("Naelson basssss");
 		p.setLname("Douglas2");		
 		
 		session.save(p);
@@ -62,15 +42,9 @@ SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		
 		session.close();
 		
-<<<<<<< HEAD
          
       		
 			
-=======
-		
-		
-		
->>>>>>> ab06cfd3286c38712a045a36cb750825862aa177
 	}
 
 }
