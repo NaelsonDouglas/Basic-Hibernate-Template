5651677a761a480d31f1c09141436e1ae6039f27
Naelson Douglas <naelson17@gmail.com>
2016-06-02 21:28:50 -0300

    added the missing files on the last commit

---
+++ src/main/java/FirstHibernate/com/myhib/CRUDS/Material.java
@@ -0,0 +1,29 @@
+package FirstHibernate.com.myhib.CRUDS;
+
+public class Material {
+	int id;
+	String brand;
+	int size;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+}
