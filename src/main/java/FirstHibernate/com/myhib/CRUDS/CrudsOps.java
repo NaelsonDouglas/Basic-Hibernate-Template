4fb2057c3f9e89b7ed8ebe4a8fba058cf962ff87
Naelson Douglas <naelson17@gmail.com>
2016-10-21 06:16:40 -0300

    Changed the code to use it as the DRCA project

--- src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
+++ src/main/java/FirstHibernate/com/myhib/CRUDS/CrudsOps.java
@@ -7,36 +7,90 @@ import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.classic.Session;
+
+
+import administrativo.Departamento;
+import administrativo.Departamento4;
+import administrativo.Disciplina;
+import administrativo.Secretaria;
+import pessoal.Aluno;
+import pessoal.Pessoa;
+import pessoal.Professor;
 
 public class CrudsOps {
 
 	public static void main(String[] args) {
 			
-SessionFactory factory = new Configuration().configure().buildSessionFactory();	
 		
-		Session session = factory.openSession();		
-		
-		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();	
+		org.hibernate.Session session = factory.openSession();		
 		Transaction t =  (Transaction) session.beginTransaction();
 		
-		Material p = new Material();		
-		p.setId(3);
-		p.setBrand("Naelson basssss");
-		p.setSize(10);		
-		session.save(p);
+		
+		//session.save(p);
+		//session.getTransaction().commit();				
+		
+		Professor a = new Professor();
+		a.setMatricula(10);
+		a.setNome("satã");
+		
+		session.save(a);
+		session.getTransaction().commit();
+		
+		Query query = session.createQuery("from Professor");		
+		System.out.println(query.list().size());
+		
+		List<Pessoa>  users =   query.list();
+		
+		for (Pessoa temp : users){
+			System.out.println(temp.getNome());
+		}
+		//------
+		
+		
+		session = factory.openSession();
+		
+		
+		Secretaria sec = new Secretaria();
+		sec.setDisciplinas(1);
+		sec.setId(2);
+		
+		
+		//Departamento4 d = new Departamento4();
+		//d.setId(14);
+		//d.setSecGraduacao_id(sec);
+		
+		
+		session.save(sec);
+		session.getTransaction().begin();
 		session.getTransaction().commit();
 		
 		
 		
-		Query query = session.createQuery("from Material");		
-		System.out.println(query.list().size());
 		
-		List<Material>  users =   query.list();
 		
-		for (Material temp : users){
-			System.out.println(temp.getBrand());
-		}
+		
+		
 		
 		
 		
