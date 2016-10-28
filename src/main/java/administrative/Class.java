0fc4ba6a29d619ae12643f51c02942ec2ada279a
Cristiano Lira <chenriquelira@gmail.com>
2016-10-28 19:16:52 -0300

    Whatever.

--- src/main/java/administrative/Class.java
+++ src/main/java/administrative/Class.java
@@ -5,10 +5,7 @@ import java.util.Set;
 import personal.Student;
 import personal.Teacher;
 
-
-
-public class Class
-{
+public class Class {
 	
 	protected String name;	
 	protected String ID;	
@@ -20,11 +17,22 @@ public class Class
 	protected String teacher;	
 	protected Set<Student> students;
 	
-	
+	public Class(String name, String iD, int credits, boolean isElective, boolean isAvailable, int minCredits,
+			Set<Class> prerequisites, String teacher, Set<Student> students) {
+		this.name = name;
+		ID = iD;
+		this.credits = credits;
+		this.isElective = isElective;
+		this.isAvailable = isAvailable;
+		this.minCredits = minCredits;
+		this.prerequisites = prerequisites;
+		this.teacher = teacher;
+		this.students = students;
+	}
 	
 	public String toString(){
 		String output = "";
-		output = "Nome: " + name + "\n" + "Código: " + ID+"\n" + "Créditos: " + Integer.toString(credits) + "\n" + "Eletiva: " + booleanToSimNao(isElective()) + "\n" +
+		output = "Nome: " + name + "\n" + "Código: " + ID + "\n" + "Créditos: " + Integer.toString(credits) + "\n" + "Eletiva: " + booleanToSimNao(isElective()) + "\n" +
 		"Disponível: " + booleanToSimNao(isAvailable()) + "\n" + "Créditos necessários: " + Integer.toString(getMinCredits()) + "\n"+"TODO PREREQUISITOS" + "\n" + "Professor: " + getTeacher() + "\n";
 		return output;		
 		
@@ -38,174 +46,77 @@ public class Class
 		}
 	}
 
-	public Class(){
-		super();
-	}
-
-
-
-
-
 	public String getName() {
 		return name;
 	}
-
-
-
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
-
-
 	public String getID() {
 		return ID;
 	}
-
-
-
-
 
 	public void setID(String iD) {
 		ID = iD;
 	}
 
-
-
-
-
 	public int getCredits() {
 		return credits;
 	}
-
-
-
-
 
 	public void setCredits(int credits) {
 		this.credits = credits;
 	}
 
-
-
-
-
 	public boolean isElective() {
 		return isElective;
 	}
-
-
-
-
 
 	public void setElective(boolean isElective) {
 		this.isElective = isElective;
 	}
 
-
-
-
-
 	public boolean isAvailable() {
 		return isAvailable;
 	}
-
-
-
-
 
 	public void setAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
 
-
-
-
-
 	public int getMinCredits() {
 		return minCredits;
 	}
-
-
-
-
 
 	public void setMinCredits(int minCredits) {
 		this.minCredits = minCredits;
 	}
 
-
-
-
-
 	public Set<Class> getPrerequisites() {
 		return prerequisites;
 	}
-
-
-
-
 
 	public void setPrerequisites(Set<Class> prerequisites) {
 		this.prerequisites = prerequisites;
 	}
 
-
-
-
-
 	public String getTeacher() {
 		return teacher;
 	}
-
-
-
-
 
 	public void setTeacher(String teacher) {
 		this.teacher = teacher;
 	}
 
-
-
-
-
 	public Set<Student> getStudents() {
 		return students;
 	}
 
-
-
-
-
 	public void setStudents(Set<Student> students) {
 		this.students = students;
 	}
-
-
-
-
-
-	public Class(String name, String iD, int credits, boolean isElective, boolean isAvailable, int minCredits,
-			Set<Class> prerequisites, String teacher, Set<Student> students) {
-		super();
-		this.name = name;
-		ID = iD;
-		this.credits = credits;
-		this.isElective = isElective;
-		this.isAvailable = isAvailable;
-		this.minCredits = minCredits;
-		this.prerequisites = prerequisites;
-		this.teacher = teacher;
-		this.students = students;
-	}
-	
-	
-	
 
 }
 
