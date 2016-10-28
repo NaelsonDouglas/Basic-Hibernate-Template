e28feec745616c93ce2161dacb06db6374614796
Naelson Douglas C. Oliveira <naelson17@gmail.com>
2016-10-28 18:02:32 -0300

    Update Generators.java

--- src/main/java/dataGenerators/Generators.java
+++ src/main/java/dataGenerators/Generators.java
@@ -1,4 +1,5 @@
 package dataGenerators;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,8 +10,8 @@ import personal.Teacher;
 public class Generators {
 	
 	
-	public Set<Departament> generateDepartments(){		
-		Set<Departament>  departments = new HashSet<Departament>();
+	public static ArrayList<Departament> generateDepartments(){		
+		ArrayList<Departament>  departments = new ArrayList<Departament>();
 		
 		
 		
@@ -40,23 +41,65 @@ public class Generators {
 		
 		
 		//Cria o set para organizar as matérias de bacharelado de computação e adciona as matérias nele
-		Set<Class> bachInfoGreadClasses= new HashSet<Class>();
-		bachInfoGreadClasses.add(introCienciaDaComputacao);
-		bachInfoGreadClasses.add(calculoNumerico);
-		bachInfoGreadClasses.add(softwareBasico);
+		Set<Class> bachInfoGradClasses= new HashSet<Class>();
+		bachInfoGradClasses.add(introCienciaDaComputacao);
+		bachInfoGradClasses.add(calculoNumerico);
+		bachInfoGradClasses.add(softwareBasico);
 		
 		//Cria o curso de engenharia de computação com as matérias		
-		Course bachareladoComputacao = new Course("Engenharia de computação", bachInfoGreadClasses);		
+		Course bachareladoComputacao = new Course("Engenharia de computação", bachInfoGradClasses);	
+		
+		//--------------------------organiza todos os cursos de graduação em uma lista----------------------------
+		
+		Set<Course> gradInfoCourses = new HashSet<Course>();
+		gradInfoCourses.add(bachareladoComputacao);
+		gradInfoCourses.add(engComputacao);		
+		//---------------------------------------------------------------------------------------------------------
+		
+		//Cria as matérias do doutorado de informática
+		Class hipermidiaAdaptativa = new Class("Hipermídia adaptativa","INF 1303", 30, true, true, 0, null, "Daniel Schawbe", null);
+		
+		
+		//Cria o curso de doutorado em informática
+		Set<Class> doutInfoPosClasses = new HashSet<Class>();
+		Course doutoradoInfo = new Course("Doutorado - Informática", doutInfoPosClasses);	
+		
+		
+		Set<Course> posGradInfoCourses = new HashSet<Course>();
+		posGradInfoCourses.add(doutoradoInfo);
+		
+		//Cria as duas secratarias de informática, já com os cursos inclusos
+		Secretary gradInfo = new Secretary(false,gradInfoCourses);
+		Secretary posGradInfo = new Secretary(true,posGradInfoCourses);
+		
+		//Cria o departamento de informática
+		Departament deptInfo = new Departament("Informática",0,gradInfo,posGradInfo);
 		
 		
 		
 		
+		///-----------------------------------Letras----------------------------------------------------
+		
+		Class inglesI = new Class("Inglês I","LET 1501", 30, false, true, 0, null, "Ângelo Souza", null);
+		
+		//Cria o set para organizar as matérias de português-inglês e adciona as matériasnele
+		Set<Class> ptrIngGradClasses = new HashSet<Class>();
+		ptrIngGradClasses.add(inglesI);
+		
+		//Cria o curso de português-inglês com as matérias
+		Course ptrIngGrad = new Course("Letras", ptrIngGradClasses);
+		
+		Set<Course> gradLetrasCourses = new HashSet<Course>();
+		gradLetrasCourses.add(ptrIngGrad);
 		
 		
+		//Secretaria de graduação de letras
+		Secretary letrasGrad = new Secretary(false,gradLetrasCourses);
+		Departament deptLetras = new Departament("Letras",1,letrasGrad,null);
 
 		
-		Departament letras = new Departament();
-		
+		departments.add(deptLetras);
+		departments.add(deptInfo);	
 		
 		
 		
